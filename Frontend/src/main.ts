5045d673b0f4f74ed0f7785262d98a78ba6948a0
vinayak2509 <114046699+vinayak2509@users.noreply.github.com>
2024-12-21 15:08:46 +0530

    .

--- Frontend/src/main.ts
+++ Frontend/src/main.ts
@@ -1,9 +1,9 @@
 import { bootstrapApplication, provideClientHydration } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-import { provideHttpClient,withFetch } from '@angular/common/http';
+import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideHttpClient(withFetch()), provideRouter(routes), provideClientHydration()],
+  providers: [provideHttpClient(), provideRouter(routes), provideClientHydration()],
 }).catch((err) => console.error(err));
