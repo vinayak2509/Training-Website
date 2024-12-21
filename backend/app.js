024af0b3dc8239c5e9a9da9b66989bb6a50c1e4c
vinayak2509 <114046699+vinayak2509@users.noreply.github.com>
2024-12-21 17:08:06 +0530

    ,

--- backend/app.js
+++ backend/app.js
@@ -15,25 +15,6 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 
-app.use(
-  cors({
-    origin: "https://training-website.onrender.com",
-    methods: "GET, POST, DELETE, PUT, PATCH, OPTIONS",
-    allowedHeaders: ["Content-Type", "Authorization"], 
-    credentials: true, 
-  })
-);
-
-
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://training-website.onrender.com");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  res.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, PATCH, OPTIONS");
-  res.header("Access-Control-Allow-Credentials", "true");
-  next();
-});
-
-app.options("*", cors());
 
 
 app.use(bodyParser.json());
